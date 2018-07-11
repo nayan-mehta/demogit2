80268bf56e8a65eee288d2ff08a50cc290743c9f
Nayan Mehta <nayan.taurian@gmail.com>
2018-07-11 19:53:50 +0530

    Loops and arrays

---
+++ loopsandarrays.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+public class loopsandarrays {
+
+	public static void main(String[] args) {
+		
+		// for loop
+	  
+//		for(int x = 1; x < 10; x++) {
+//	         System.out.println("value of x : " + x );
+//	      }
+		
+		// for each loop
+		
+//		int marks[]= {10,20,30,40,50,60};
+		
+//		for(int mark:marks){
+//			System.out.println(mark);
+//		}
+//		
+//		
+//		for(int mark=0;mark<marks.length;mark++){
+//			
+//			System.out.println(marks[mark]);
+//			
+//		}
+//		
+		
+	   //break
+//		for (int i = 0; i < 10; i++)
+//        {
+//            if(i == 5)
+//                break;   
+//            
+//            System.out.println(i + " ");
+//        }
+		
+		//continue
+		
+//		for(int i = 0; i <= 10; i++){  
+//            if(i == 5){ 
+//                 continue;  
+//              }  
+//             System.out.println(i);  
+//         } 
+		
+		//labeled break
+		
+//		outer : for (int i = 0; i < 5; i++){
+//			
+//	           inner: for (int j = 0; j < 5; j++){
+//	                    if(j == 3) {
+//	                    	
+//	                    	  break outer;	
+//	                    	  
+//	                         }
+//	                        System.out.println("Inner"+i+" "+j);
+//	                      }   
+//	
+//		                   System.out.println("Outer"+i);
+//	            }
+		//labeled continue
+		
+//		outer : for(int i=0; i < 5; i++){
+//            inner : for(int j=0; j < 5 ; j++){
+//                        if(j == 3) {
+//                            continue outer;
+//                            }
+//                        System.out.println(j);
+//                    }
+//                }
+	   //Arrays
+//		int arr[] = new int[5];
+//		System.out.println(arr[4]);
+//		Scanner sc = new Scanner(System.in); 
+//		
+//		for(int i=0;i<arr.length;i++) { 
+//			System.out.println("Enter a number: ");  
+//			int number=sc.nextInt(); 
+//		    arr[i]=number;
+//		}
+//		sc.close();
+//		System.out.println(arr[4]);
+		
+		//Console input
+		System.out.print(args[0]);
+	}
+
+}
