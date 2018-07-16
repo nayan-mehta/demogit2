ea6cba94418f4cdec3035df7f63da1eb69ee6b92
Ankur Vashist <ankurvashist99@gmail.com>
2018-07-16 18:03:03 +0530

    Doubt
    Why it is marked incorrect in the assignment?

---
+++ Static.java
@@ -0,0 +1,17 @@
+
+public class Static {
+	
+		   static int num= 97;
+		   static String mystr = "Static keyword in Java";
+		   static void print()
+		   {
+			   System.out.println("Static Function");
+		   }
+		   public static void main(String args[])
+		   {
+			   print();
+		      System.out.println("Value of static variable: "+num);
+		      System.out.println("Value of mystr: "+mystr);
+		   }
+	
+}
