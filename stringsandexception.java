577486bfc33e7dea1355da40d845f0518d248df8
Nayan Mehta <nayan.taurian@gmail.com>
2018-07-17 19:54:10 +0530

    Strings and Exception Handling

---
+++ stringsandexception.java
@@ -0,0 +1,118 @@
+package demo;
+
+import java.io.*;
+
+class A
+{
+    void display()
+    {
+        // An Instance Method
+    }
+}
+public class stringsandexception {
+
+	public static void main(String[] args) {
+	
+		//Declarations
+//		String str = "Hello";
+//		
+//		String s1 = new String("Hello"); 
+//		
+//		char[] hello ={'H','e','l','l','o'};
+//		String s2 = new String(hello);
+//		
+//		System.out.println(str+" "+s1+" "+s2);
+		
+		//Concatenation
+		// + operator
+//		String s3 = "abc" + "xyz";
+//        System.out.println(s3);
+//        
+//        String s4 = " "+ 40 + 50 + "Hello" + 30 + 30;
+//        System.out.println(s4);
+        
+        // concat
+//        String s1 = "Acad";  
+//        String s2 = "View";  
+//        String s3 = s1.concat(s2);  
+//        System.out.println(s3);
+		
+		//Substring
+		
+//		 String s = "YelloPeople";  
+//         String sub1 = s.substring(5);
+//         System.out.println(sub1);
+//         String sub2 = s.substring(0,5);
+//         System.out.println(sub2);
+		
+		//String Comparison
+		//	equal()
+//		String s1 = "Nayan";  
+//        String s2 = "Nayan";  
+//        String s3 = new String("Nayan");  
+//        String s4 = "NayaN";  
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1.equals(s3));
+//        System.out.println(s1.equals(s4));
+        //	==
+//		 String s1 = "Nayan";  
+//         String s2 = "Nayan";  
+//         String s3 = new String("Nayan");  
+//	     System.out.println(s1 == s2);//true (both refer to same instance)  
+//	     System.out.println(s1 == s3);//false(s3 refers to instance created 
+//	                                 //in non-pool area) 
+		//compareTo
+		
+//		String s1="Nayan";  
+//        String s2="Nayan";  
+//        String s3="NayanMehta";  
+//        System.out.println(s1.compareTo(s2)==0);
+//        System.out.println(s3.compareTo(s2)<0);
+//        System.out.println(s3.compareTo(s2)>0);
+		
+		//StringBuffer
+//		StringBuffer sb1 = new StringBuffer("abc");
+//        
+//		sb1.append("Lalal");
+//        System.out.println(sb1);
+//        
+//        sb1.insert(5, "for");
+//        System.out.println(sb1);
+////        System.out.println(sb1.length());
+////        System.out.println(sb1.capacity());
+//        System.out.println(sb1.reverse());
+        
+//        StringBuffer sb2 = new String("xyz");
+//        String s3 = new StringBuffer("abc");
+		
+		//Exception Handling
+		
+//		int a = 5, b = 2, c = 2, d;
+//        
+//        d = a/(b-c); // ArithmeticException(Divide By Zero) Occurred
+//        
+//        System.out.println(d); // This will not print
+		
+//		A a1 = null;
+//        a1.display(); // Null Pointer Exception
+//        
+//        System.out.println("This will not print");
+//		
+//		try {
+//			int a = 5, b = 2, c = 2, d;
+//	        System.out.println("Before Exception"); 
+//	        d = a/(b-c); // ArithmeticException(Divide By Zero) Occurred
+//	        System.out.println("This will not print"); // This will not print
+//	    } catch(Exception e) 
+//	    {
+//	        System.out.println("After Exception");
+//	        System.out.println("exception : Division by zero");
+////	        System.out.println(e.getMessage());
+////	        System.out.println(e); //toString() is called
+////	        e.printStackTrace();
+//	    }   
+    
+        
+	}
+
+}
