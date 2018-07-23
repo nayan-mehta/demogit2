1d5aa4d324f89a35ebc65d6eb1b861b25bf9a949
Nayan Mehta <nayan.taurian@gmail.com>
2018-07-23 19:11:40 +0530

    Collection2- Vector, Sets and Maps

---
+++ Collections2.java
@@ -0,0 +1,70 @@
+package demo;
+
+import java.util.*;
+
+public class Collections2 {
+
+	public static void main(String[] args) {
+		
+		//Vector Basics
+//		Vector v = new Vector();
+//       
+//        v.add(1);
+//        v.add(2);
+//        v.add("Lala");
+//        v.add("Deedeeedee");
+//        v.add(3);
+//        
+//        System.out.println("Vector is " + v);
+        
+        
+        //HashSets
+        
+//        HashSet<String> h = new HashSet<String>();
+//        
+//        // Adding elements into HashSet usind add()
+//        h.add("India");
+//        h.add("Australia");
+//        h.add("South Africa");
+//        h.add("India");// adding duplicate elements
+// 
+//        // Displaying the HashSet
+//        System.out.println(h);
+//        System.out.println("List contains India or not:" +
+//                           h.contains("India"));
+// 
+//        // Removing items from HashSet using remove()
+//        h.remove("Australia");
+//        System.out.println("List after removing Australia:"+h);
+// 
+//        // Iterating over hash set items
+//        System.out.println("Iterating over list:");
+//        Iterator<String> i = h.iterator();
+//        while (i.hasNext())
+//            System.out.println(i.next());
+		
+		
+		//HashMap
+//		HashMap hm=new HashMap();
+//        hm.put("MSD",new Double(2218.40));
+//        hm.put("SSA",new Double(8122.40));
+//        hm.put("MSS",new Double(4037.22));
+//        hm.put("MSR",new Double(3740.37));
+//        hm.put("SRK",new Double(4730.18));
+//        
+//        
+//        Set set=hm.entrySet();
+//        System.out.println(set);
+//        Iterator i=set.iterator();
+//        
+//        while(i.hasNext()){
+//            Map.Entry me=(Map.Entry)i.next();
+//            System.out.println(me.getKey()+":");
+//            System.out.println(me.getValue()+":");
+//        }
+        
+        
+        
+	}
+
+}
